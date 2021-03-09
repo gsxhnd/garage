5a5a37d7114973266527347a67bb49fc680b2e52
gsxhnd <gsxhnd@gmail.com>
2021-03-09 11:37:03 +0800

    feat: add dao

--- dao/jav_star.go
+++ dao/jav_star.go
@@ -1,12 +1,10 @@
 package dao
 
-import (
-	"garage/model"
-)
+import "garage/model"
 
-func CreateJavMovie(jm *[]model.JavMovie) error {
+func CreateJavStar(js *[]model.JavStar) error {
 	db := Database.Default
-	row := db.Create(&jm)
+	row := db.Create(&js)
 	if row.Error != nil {
 		return nil
 	} else {
@@ -14,15 +12,15 @@ func CreateJavMovie(jm *[]model.JavMovie) error {
 	}
 }
 
-func GetJavMovie() (interface{}, error) {
+func GetJavStarList() (interface{}, error) {
 	var (
 		db   = Database.Default
 		data = struct {
-			Total int64            `json:"total"`
-			List  []model.JavMovie `json:"list"`
+			Total int64           `json:"total"`
+			List  []model.JavStar `json:"list"`
 		}{}
 	)
-	row := db.Model(&model.JavMovie{}).
+	row := db.Model(&model.JavStar{}).
 		Count(&data.Total).
 		Find(&data.List)
 	if row.Error != nil {
