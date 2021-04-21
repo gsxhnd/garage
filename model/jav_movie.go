ae9874d6bc4b0c9dfbf1ca3d32105608aad18017
gsxhnd <gsxhnd@gmail.com>
2021-04-21 11:59:41 +0800

    fix: model sql type

--- model/jav_movie.go
+++ model/jav_movie.go
@@ -1,10 +1,12 @@
 package model
 
+import "time"
+
 type JavMovie struct {
-	Code        string `json:"code" gorm:"type:varchar(100);primaryKey"` // 番号
-	Title       string `json:"title" gorm:"type:text"`                   // 名称
-	PublishDate string `json:"publish_date" gorm:"type:varchar(100)"`    // 发布时间
-	Director    string `json:"director" gorm:"type:varchar(100)"`        // 导演
+	Code        string    `json:"code" gorm:"type:varchar(100);primaryKey"` // 番号
+	Title       string    `json:"title" gorm:"type:text"`                   // 名称
+	PublishDate time.Time `json:"publish_date" gorm:"type:datetime"`        // 发布时间
+	Director    string    `json:"director" gorm:"type:varchar(100)"`        // 导演
 }
 
 func (jm *JavMovie) TableName() string {
