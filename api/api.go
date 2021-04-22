695cbe3bf06525282983b47ffd2ca0e3ad442191
gsxhnd <gsxhnd@gmail.com>
2021-04-22 18:45:09 +0800

    refactor: delete not used code

--- api/api.go
+++ api/api.go
@@ -1,30 +1,9 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
-	"os/exec"
-	"runtime"
 	"time"
 )
-
-func OpenBrowser(url string) {
-	var commands = map[string]string{
-		"windows": "start",
-		"darwin":  "open",
-		"linux":   "xdg-open",
-	}
-	run, ok := commands[runtime.GOOS]
-	if !ok {
-		fmt.Printf("don't know how to open things on %s platform\n", runtime.GOOS)
-	}
-
-	cmd := exec.Command(run, url)
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println(err)
-	}
-}
 
 func Run(port, imgDir string) error {
 	srv := &http.Server{
