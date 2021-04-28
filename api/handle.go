7a1cb7e08e44ebfeaf35cbf6eb645a0092a8b2bd
gsxhnd <gsxhnd@gmail.com>
2021-04-28 18:40:39 +0800

    feat: add api

--- api/handle.go
+++ api/handle.go
@@ -14,10 +14,17 @@ func GetJavStar(w http.ResponseWriter, req *http.Request) {
 
 }
 
+func DeleteJavMovie(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func GetJavMovieInfo(w http.ResponseWriter, req *http.Request) {
+
+}
+
 func CrawlJavMovie(w http.ResponseWriter, req *http.Request) {
 	SendRes(w, nil, "movie")
 }
-
 
 func CrawlJavStar(w http.ResponseWriter, req *http.Request) {
 	SendRes(w, nil, "movie")
--- api/route.go
+++ api/route.go
@@ -11,12 +11,14 @@ func routes(imgDir string) *mux.Router {
 	r.Use(AuthMiddleware(owl.GetString("dashboard.key"), owl.GetString("dashboard.secret")).Middleware)
 	r.PathPrefix("/img/").Handler(http.StripPrefix("/img/", http.FileServer(http.Dir(imgDir))))
 	api := r.PathPrefix("/v1/api").Subrouter()
-	api.HandleFunc("/jav/movie", GetJavMovie).Methods("GET")          // get jav movie list
-	api.HandleFunc("/jav/movie", UpdateJavMovie).Methods("PUT")       // update jav movie info
-	api.HandleFunc("/jav/star", GetJavStar).Methods("GET")            // get jav star list
-	api.HandleFunc("/crawl/jav/movie", CrawlJavMovie).Methods("POST") // crawl jav movie info
-	api.HandleFunc("/crawl/jav/star", CrawlJavStar).Methods("POST")   // crawl jav star info
-	api.HandleFunc("/version", GetVersion).Methods("GET")             // crawl jav star info
+	api.HandleFunc("/jav/movie", GetJavMovie).Methods("GET")            // get jav movie list
+	api.HandleFunc("/jav/movie/{code}", GetJavMovieInfo).Methods("GET") // get jav movie info include star and tag's list
+	api.HandleFunc("/jav/movie", UpdateJavMovie).Methods("PUT")         // update jav movie info
+	api.HandleFunc("/jav/movie", DeleteJavMovie).Methods("DELETE")      // update jav movie info
+	api.HandleFunc("/jav/star", GetJavStar).Methods("GET")              // get jav star list
+	api.HandleFunc("/crawl/jav/movie", CrawlJavMovie).Methods("POST")   // crawl jav movie info
+	api.HandleFunc("/crawl/jav/star", CrawlJavStar).Methods("POST")     // crawl jav star info
+	api.HandleFunc("/version", GetVersion).Methods("GET")               // server version
 
 	return r
 }
