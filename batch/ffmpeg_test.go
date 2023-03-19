2de0810d2d59c89f67ad0c39397072cd10251d57
gsxhnd <gsxhnd@gmail.com>
2023-03-19 19:44:06 +0800

    test

--- batch/ffmpeg_test.go
+++ batch/ffmpeg_test.go
@@ -2,6 +2,8 @@ package batch
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestVideoBatch_GetVideos(t *testing.T) {
@@ -12,24 +14,24 @@ func TestVideoBatch_GetVideos(t *testing.T) {
 		want            []string
 		wantErr         bool
 	}{
-		{"test_mkv", "../testdata", ".mkv", []string{"1"}, false},
-		{"test_mp4", "../testdata", ".mp4", []string{"1"}, false},
+		{"test_mkv", "../testdata", ".mkv", []string{"1"}, true},
+		{"test_mp4", "../testdata", ".mp4", []string{"1"}, true},
 		{"test_err", "./test", ".mp4", []string{}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// vb := new(VideoBatch)
-			// vb.SourceRootPath = tt.sourceRootPath
-			// vb.SourceVideoType = tt.sourceVideoType
+			vb := new(VideoBatch)
+			vb.SourceRootPath = tt.sourceRootPath
+			vb.SourceVideoType = tt.sourceVideoType
 
-			// got, err := vb.GetVideos()
-			// if err != nil {
-			// 	if tt.wantErr {
-			// 		assert.Error(t, err)
-			// 	}
-			// } else {
-			// 	assert.Equal(t, tt.want, got)
-			// }
+			got, err := vb.GetVideos()
+			if err != nil {
+				if tt.wantErr {
+					assert.Error(t, err)
+				}
+			} else {
+				assert.Equal(t, tt.want, got)
+			}
 		})
 	}
 }
