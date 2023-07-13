ee5e2bcaf1c011568659c5ee99bab9d70bac4c3e
gsxhnd <gsxhnd@gmail.com>
2023-07-13 13:35:28 +0800

    feat: add version cmd

--- src/cmd/version.go
+++ src/cmd/version.go
@@ -2,15 +2,27 @@ package cmd
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/urfave/cli/v2"
+)
+
+var (
+	tag       string
+	buildDate string
+	gitCommit string
 )
 
 var versionCmd = &cli.Command{
 	Name:  "version",
 	Usage: "显示版本号",
 	Action: func(context *cli.Context) error {
-		fmt.Println("version cmd")
+		fmt.Println("Version: ", tag)
+		fmt.Println("BuildDate:", buildDate)
+		fmt.Println("GitCommit:", gitCommit)
+		fmt.Println("GoVersion:", runtime.Version())
+		fmt.Println("Compiler:", runtime.Compiler)
+		fmt.Println("Paltform:", fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH))
 		return nil
 	},
 }
