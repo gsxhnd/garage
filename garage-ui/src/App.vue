4246062d539fa985a83db75d340edb099650f3cf
gsxhnd <gsxhnd@gmail.com>
2024-02-23 13:19:43 +0800

    update

--- garage-ui/src/App.vue
+++ garage-ui/src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import { testStore } from "@/stores/index";
+import { userLogin, testApi } from "@/api/user";
 
 testStore();
+userLogin();
+testApi().catch((err) => {
+  console.log(err);
+});
 </script>
 
 <template>
--- garage_di/app.go
+++ garage_di/app.go
@@ -1,9 +1,12 @@
 package garage_di
 
 import (
+	"fmt"
 	"io/fs"
 	"log"
 	"net/http"
+	"os/exec"
+	"runtime"
 
 	"github.com/gin-gonic/gin"
 	garage_ui "github.com/gsxhnd/garage/garage-ui"
@@ -13,6 +16,23 @@ import (
 
 type Application struct {
 	router *routes.Routes
+}
+
+// Open calls the OS default program for uri
+func Open(uri string) error {
+	var commands = map[string]string{
+		"windows": "cmd /c start",
+		"darwin":  "open",
+		"linux":   "xdg-open",
+	}
+
+	run, ok := commands[runtime.GOOS]
+	if !ok {
+		return fmt.Errorf("don't know how to open things on %s platform", runtime.GOOS)
+	}
+
+	cmd := exec.Command(run, uri)
+	return cmd.Start()
 }
 
 func NewApplication(r *routes.Routes) *Application {
@@ -40,6 +60,8 @@ func (a *Application) Run() error {
 	g.Go(func() error {
 		return a.router.Engine.Run("0.0.0.0:8080")
 	})
+
+	Open("http://localhost:8081")
 
 	if err := g.Wait(); err != nil {
 		return err
