ee561759d5f60af0f6029ad143d13642f984fdff
gsxhnd <gsxhnd@gmail.com>
2024-04-07 21:35:37 +0800

    server: resp movce to model

--- garage_server/model/resp.go
+++ garage_server/model/resp.go
@@ -1,9 +1,7 @@
-package resp
+package model
 
 type Response struct {
 	Code    int         `json:"code"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
 }
-
-func NewResponse(code int, message string, data interface{}) {}
